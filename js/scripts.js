86d5b7cd2bec6b55f9f1a80a52b5b0bc3425d84a
Marcin Doliwa <mdoliwa@gmail.com>
2020-08-14 23:54:50 +0200

    Add drawSingleGrid function

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,14 @@ canvas.width = cellWidth * columns;
 console.log("cellWidth: ", cellWidth);
 
 drawGrid();
+drawSingleGrid();
+drawMenu();
 
 
 function drawGrid() {
   const prevStrokeStyle = ctx.strokeStyle;
 
-  ctx.strokeStyle = '#cbd5e0';
+  ctx.strokeStyle = '#e2e8f0';
 
   for (var i = 0; i < columns + 1; i++) {
     ctx.beginPath();
@@ -34,6 +36,39 @@ function drawGrid() {
   }
 
   ctx.strokeStyle = prevStrokeStyle;
+}
+
+function drawSingleGrid() {
+  const prevStrokeStyle = ctx.strokeStyle;
+
+  ctx.strokeStyle = '#a0aec0';
+
+	for (var i = 0; i < columns + 1; i++) {
+		ctx.beginPath();
+		ctx.moveTo(i * cellWidth, 0);
+		ctx.lineTo(i * cellWidth, rows / 2 * cellWidth);
+		ctx.stroke();
+		ctx.closePath();
+	}
+
+	for (var j = 0; j < rows / 2 + 1; j++) {
+		ctx.beginPath();
+		ctx.moveTo(0, j * cellWidth);
+		ctx.lineTo(columns * cellWidth, j * cellWidth);
+		ctx.stroke();
+		ctx.closePath();
+	}
+
+  ctx.strokeStyle = prevStrokeStyle;
+}
+
+function drawMenu() {
+	var menu = document.getElementById("menu");
+
+	menu.style.top = '0px'; //canvas.height / 4 - menu.offsetHeight / 2 + 'px';
+	menu.style.left = '0px'; //canvas.width / 2 - menu.offsetWidth / 2 + 'px';
+	menu.style.width = canvas.width + 'px';
+	menu.style.height = cellWidth * rows + 'px'
 }
 
 
